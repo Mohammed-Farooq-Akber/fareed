90bb67ec13a2c062e41bd3d620a125909671e6b7
Mohammed-Farooq-Akber <test.firewall.test@gmail.com>
2026-04-19 12:31:16 +0530

    Delete public/JS directory

--- public/JS/form.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("intakeForm").addEventListener("submit", async(e)=>{
-e.preventDefault();
-const formData=new FormData(e.target);
-await fetch("/api/intake",{method:"POST",body:formData});
-alert("Submitted");
-});
